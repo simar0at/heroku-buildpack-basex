8320875d57da12921334f2d7fbc65a92e92021d6
Omar Siam <Omar.Siam@oeaw.ac.at>
2022-03-01 21:52:16 +0100

    Update defaults to latest avalable versions It seems a xmlresolver jar is not availabel in all 9.x versions of exist, fetch this to custom

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -166,7 +166,7 @@ install_jdk() {
 }
 
 install_basex() {
-  local version=${1:-9.5.1}
+  local version=${1:-9.6.4}
   local install_dir=${2}
   local basexzip=BaseX${version//.}.zip
 
@@ -177,7 +177,8 @@ install_basex() {
 }
 
 install_saxon() {
-  local version=${1:-10.3}
+  local version=${1:-11.2}
   local install_dir=${2}
+  curl --retry 3 --location -s "https://repo1.maven.org/maven2/org/xmlresolver/xmlresolver/4.2.0/xmlresolver-4.2.0.jar" --output "${install_dir}/lib/custom/xmlresolver-4.2.0.jar"
   curl --retry 3 --location -s "https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/${version}/Saxon-HE-${version}.jar" --output "${install_dir}/lib/custom/Saxon-HE-${version}.jar"
 }
