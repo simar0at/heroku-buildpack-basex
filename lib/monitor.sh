76081f66ca3c509152492039c24a161ed4d2315a
Omar Siam <Omar.Siam@oeaw.ac.at>
2024-01-10 13:46:38 +0100

    Debug monitor

--- lib/monitor.sh
+++ lib/monitor.sh
@@ -6,6 +6,8 @@ monitor_memory_usage() {
   # drop the first argument, and leave other arguments in place
   shift
 
+  export TRACE=${$TRACE:-false}
+  echo "TRACE=$TRACE"
   # Run the command in the background
   "${@:-}" &
 
